b6205134ebbfb405a5f0a67e27d58fd12d64c7af
terimiyaki <wackoli123@yahoo.com>
2014-10-21 22:50:11 -0400

    got the numbers to generate randomly

--- app.js
+++ app.js
@@ -1,45 +1,73 @@
-var canvas, context;
+var canvas,context;
+var gameObjects = {};
 
 window.onload = function(){
-init();
-}
+	init();
+};
 
 function init() {
 	canvas = document.getElementById('playArea');
 	context = canvas.getContext('2d');
-	render();
+	setInterval(render,1000);
+}
+
+function createGameObjects(){
+
 }
 
 function render(){
-		var x = canvas.width / 2;
-      	var y = canvas.height / 2;
+		//loop
+		context.save();
+		contextClear();
 
-      	var number = Math.floor(Math.random() * (11 - 1)) + 1;
-
-      	context.font = 'bold 48pt sans-serif';
-      	context.textAlign = 'center';
-      	context.fillStyle = 'black';
-      	context.fillText(number, x, y);
-
+		generateTarget();
+		generateNumber();
+      	context.restore();
 }
 
 function keydown(e){
 	//Checks what Keys were pressed
+	switch (e.keyCode) {
+		case 32: //Spacebar
+			break;
+		case 38: //Up key
+			break;
+		case 40: //Down key
+			break;
+	}
+}
+
+function contextClear(){
+	context.clearRect(0,0,500,400);
 }
 
 function generateTarget(){
 	//Creates random target number
+	var x = canvas.width / 2;
+	var y = canvas.height / 8;
+	var number = Math.floor(Math.random() * (11 - 1)) + 1;
+	context.font = "bold 32pt sans-serif";
+	context.textAlign = "center";
+	context.fillStyle = "gray";
+	context.fillText(number, x, y);
 }
 
 function generateNumber(){
 	//Creates random number
+	var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    var number = Math.floor(Math.random() * (11 - 1)) + 1;
+    context.font = 'bold 56pt sans-serif';
+    context.textAlign = 'center';
+    context.fillStyle = 'black';
+    context.fillText(number, x, y);
 }
 
 function gameStore(){
 	//Manages store that sells powerups
 }
 
-function collision(){
+function checksCollision(){
 	//Checks if the target number was hit and what happens afterwards
 }
 
